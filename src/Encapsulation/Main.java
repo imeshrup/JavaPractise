1dd3babcec515bfbee0c377d6e37f076339c3860
Denuka <imeshrupasinghe50@gmail.com>
2023-08-30 07:40:14 +0530

    Encapsulation

---
+++ src/Encapsulation/Main.java
@@ -0,0 +1,11 @@
+package Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        UStudent onj = new UStudent();
+        onj.setName("kamal");
+        System.out.println(onj.getName());
+        onj.setAge(26);
+        System.out.println(onj.getAge());
+    }
+}
---
+++ src/Encapsulation/UStudent.java
@@ -0,0 +1,19 @@
+package Encapsulation;
+
+public class UStudent {
+    private String name;
+    private int age;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+}
