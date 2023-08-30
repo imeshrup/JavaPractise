5585da174259c6fa72e876983d9bbecf9c299198
Denuka <imeshrupasinghe50@gmail.com>
2023-08-30 07:17:30 +0530

    This keyword

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    String name;
+    int age;
+
+    Student(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    public static void main(String[] args) {
+        Student obj = new Student("imesh",26);
+        System.out.println(obj.name);
+        System.out.println(obj.age);
+    }
+}
